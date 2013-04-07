cae712f7d3ab2623dae33da22fcb50e08ace2f8e
David Robertson <david@davidr.me>
2013-04-08 00:43:34 +0100

    Initial commit, blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picawesome::Application.config.secret_token = 'dba79d3b2e0b8519c15eff0be00e2d47edbf15ebbeb1f42b94f01eb0b1188b644c5c0ab26a2ffc9fc75cf5171c546430f492bb987d64819eb8452f64179cd090'
