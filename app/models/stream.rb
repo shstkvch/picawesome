bbbd56380448522f3e80ba470f7f9a8e2e34e858
David Robertson <david@davidr.me>
2013-04-08 03:15:26 +0100

    Validation (very basic)

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,5 +1,8 @@
 class Stream < ActiveRecord::Base
   attr_accessible :slug, :upvote_copy, :downvote_copy, :background_color, :nsfw
+  validates :slug, presence: true
+  validates :slug, uniqueness: { case_sensitive: false }
+  # validates :slug_begins_with_letter #TODO: write this validator when you can be bothered.
 
   has_many :pics
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :username
+  validates :username, presence: true
+  validates :username, uniqueness: { case_sensitive: false }
+
 
   has_many :pics
   has_many :votes
